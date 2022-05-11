78f463eb975bcebe4f183eda2396b86aa8f69eef
martynhorslen <2880060+MartynHorslen@users.noreply.github.com>
2022-05-11 16:45:55 +0100

    Testing

--- js/animation.js
+++ js/animation.js
@@ -1,24 +1,8 @@
 const getImage = async (search) => {
     let url = "https://loremflickr.com/json/1920/1080/" + search + "/all";
-    // const res = await fetch(url, {
-    //     headers: {
-    //       "Access-Control-Allow-Origin": '*'
-    //     }
-    //    });
-    // const json = await res.json();
-    // return json;
-    return $.get("https://loremflickr.com/json/1920/1080/", () => {
-        alert("success");
-      })
-      .done(function() {
-        alert( "second success" );
-      })
-      .fail(function() {
-        alert( "error" );
-      })
-      .always(function() {
-        alert( "finished" );
-      });
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
 }
 
 const getSearchInput = () => {
